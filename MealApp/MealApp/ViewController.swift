7c650fbf863e294fb2e36907dce0985205d66ea2
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2024-01-14 07:15:46 -0800

    Add files via upload

---
+++ MealApp/MealApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealApp
+//
+//  Created by Mohit Negi on 1/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
